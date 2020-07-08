af995dbd58498ee3dbf082a7a7d9feea958bd04e
Ebuka Mbata <rockymmpr@gmail.com>
2020-07-08 17:02:14 +0100

    first commit

---
+++ testing_demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace testing_demo
+{
+    class Program
+    {
+        public int Addition(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public bool TheCheckerOfTruth(bool value)
+        {
+            return value;
+        }
+
+        public string UserName(string name)
+        {
+            return name;
+        }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+    }
+}
---
+++ testing_demo/ProgramTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+
+namespace testing_demo
+{
+    public class ProgramTests
+    {
+        Program program = new Program();
+
+        [Fact]
+        public void GoodTest()
+        {
+            Assert.Equal(10, program.Addition(1, 9)); // asserting that the sum of numbers equal 10
+        }
+
+        [Fact]
+        public void BadTest()
+        {
+            Assert.Equal(10, program.Addition(10, 90)); // asserting that the sum of numbers equal 100
+        }
+
+        [Fact]
+        public void TruthChecker()
+        {
+            Assert.True(program.TheCheckerOfTruth(true));
+        }
+
+        [Fact]
+        public void NameChecker()
+        {
+            Assert.Contains("Ebuka", program.UserName("Ebuka"));
+        }    
+    }
+}
